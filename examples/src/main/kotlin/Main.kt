8ac26fca32d7f298873489065c61182b83e7ac76
MohammadL <mmlaif@gmail.com>
2020-02-29 21:36:27 +0300

    Add Coroutine Dependencies

--- examples/src/main/kotlin/Main.kt
+++ examples/src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+
 fun main() {
     println("Hello World")
+
+    GlobalScope.launch { println("Hello World from Coroutine") }
+
+    Thread.sleep(100)
 }

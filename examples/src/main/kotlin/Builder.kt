fd95df513f348e990f6cb019cfdce6ec5daf9abf
MohammadL <mmlaif@gmail.com>
2020-03-06 08:30:45 +0300

    Add launch Builder example

--- examples/src/main/kotlin/Builder.kt
+++ examples/src/main/kotlin/Builder.kt
@@ -1,10 +1,21 @@
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 fun main() {
+
+    // runBlocking Builder
     runBlocking {
         println("Hello World")
     }
-    
+
+
+    // launch Builder
+    GlobalScope.launch(Dispatchers.Default) {
+        println("Hello World from Coroutine")
+    }
+
+    // Or
+    val myJob: Job = GlobalScope.launch(Dispatchers.Default) {
+        println("Hi from Coroutine 2")
+    }
+
 }
